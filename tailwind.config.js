a8f8f61531b87a8addbdcc1e8b8b6ada2c58660b
Gabriela <yigaschdev@gmail.com>
2024-10-20 11:25:52 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'petblue': '#7892F1',
+      },
+      fontFamily: {
+        roboto: ['Roboto Flex', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
